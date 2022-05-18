cc7c8aea3e7c0b8cfc230f6796eedd543df6da65
Jose1060 <84330500+Jose1060@users.noreply.github.com>
2022-05-18 13:30:16 -0500

    Actualizado

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,42 @@
-import './styles/main.scss'
-import { fromEvent, throttleTime } from "rxjs";
+import "./styles/main.scss";
+import { fromEvent, interval, throttleTime } from "rxjs";
 
+(function () {
+	const block: HTMLElement = document.querySelector(".container");
+	const button = document.querySelector(".btn-main");
+	const title: HTMLElement = document.querySelector(".title");
 
+	button.addEventListener("click", () => {
+		console.log("hola mundo");
+		block.style.backgroundColor = "yellow";
+		title.innerHTML = "Es de dia";
+		title.style.color = "black";
+	});
 
-(function(){
-    const block:HTMLElement = document.querySelector(".block");
-    const button = document.querySelector('.btn-main')
-    button.addEventListener('click',()=>{
-        console.log('hola mundo')
-        block.style.backgroundColor="blue";
-    })
+	const observableButton$ = fromEvent(button, "click"); //Observable
 
+	//Subscription
+	observableButton$.pipe(throttleTime(5000)).subscribe(() => {
+		console.log("en este caso cambiar el color");
+		block.style.backgroundColor = "darkblue";
+		title.innerHTML = "Es de noche";
+		title.style.color = "white";
 
-    const observableButton$ = fromEvent(button, 'click');
-    
-    observableButton$
-    .pipe(throttleTime(2000))
-    .subscribe(()=>{
-        console.log('en este caso cambiar el color')
-    })
-
-})()
-
+		// const contador = interval(1000);
+		// contador.subscribe((n) => {
+		// 	console.log("Contador", n);
+		// });
+	});
+})();
 
 /**
  * Stream: Click
- * 
- * 
+ *
+ *
  * Observable: fromEvent
- * 
- * 
- * 
- * Subscription: Escribe en consola 
- * 
+ *
+ *
+ *
+ * Subscription: Escribe en consola
+ *
  */
